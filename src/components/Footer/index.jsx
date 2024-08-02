ee2c967e1ba075d4c168032d9bfd86be3622d28e
unknown <cheenashaikh934@gmail.com>
2024-08-02 17:10:37 +0500

    routes and about us page css

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footerDesign">
+        <p>
+          <FontAwesomeIcon icon={faEnvelope} />
+          cheenashaikh934@gmail.com
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faPhone} />
+          +954-7643280-33
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
